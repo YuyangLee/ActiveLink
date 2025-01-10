d7464d3731060e722d67cd90f88d302f73fa6c13
Tim <2000017702@stu.pku.edu.cn>
2025-01-10 03:26:58 +0000

    before remove unused rules

--- synthetic_data/generate.py
+++ synthetic_data/generate.py
@@ -1,13 +1,14 @@
 import numpy as np
 from numba import jit
 import argparse
+from tqdm import tqdm
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--entity', type=int, default=125)
-parser.add_argument('--relation', type=int, default=50)
-parser.add_argument('--lambda1', type=float, default=2.0)
-parser.add_argument('--lambda2', type=float, default=0.8)
-parser.add_argument('--known', type=int, default=125)
+parser.add_argument('--entity', type=int, default=300)
+parser.add_argument('--relation', type=int, default=20)
+parser.add_argument('--lambda1', type=float, default=4)
+parser.add_argument('--lambda2', type=float, default=1)
+parser.add_argument('--known', type=int, default=1000)
 parser.add_argument('--seed', type=int, default=2024)
 parser.add_argument('--output_rule', type=str, default='rules.txt')
 parser.add_argument('--output_relation', type=str, default='relations.txt')
@@ -18,13 +19,15 @@ np.random.seed(args.seed)
 
 def generate_rule():
     total_triplets = np.random.poisson(args.lambda2) + 1
-    total_addition_entity = min(np.random.randint(0, 4), total_triplets - 1)
+    total_addition_entity = min(np.random.randint(0, 3), total_triplets - 1)
     total_relation = np.random.randint(1, min(total_triplets, args.relation) + 1)
     relations = np.random.choice(args.relation, total_relation, replace=False)
     Relation = np.random.choice(relations, total_triplets, replace=True)
     before_shuffle = np.concatenate([np.random.choice(total_addition_entity + 2, 2 * total_triplets - total_addition_entity - 2, replace=True), np.arange(total_addition_entity + 2)])
     np.random.shuffle(before_shuffle)
     Entity = before_shuffle.reshape(-1, 2)
+    if np.sum(Entity[:, 0] == Entity[:, 1]) > 0:
+        return generate_rule()
     Rule = np.concatenate([Relation.reshape(-1, 1), Entity], axis=1)
     return Rule
 
@@ -46,10 +49,12 @@ with open(args.output_rule, 'w') as file:
 
 known_relations = set()
 for i in range(args.relation):
-    known_relations.add((i, np.random.randint(args.entity), np.random.randint(args.entity)))
+    entities = np.random.choice(args.entity, 2, replace=False)
+    known_relations.add((i, entities[0], entities[1]))
 
 for _ in range(args.known - args.relation):
-    known_relations.add((np.random.randint(args.relation), np.random.randint(args.entity), np.random.randint(args.entity)))
+    entities = np.random.choice(args.entity, 2, replace=False)
+    known_relations.add((np.random.randint(args.relation), entities[0], entities[1]))
 # Delete unused entities and rename the entities to be continuous
 known_entities = set()
 for relation in known_relations:
@@ -68,47 +73,80 @@ def check_mapping(rule, mapping, knowns):
         real_entity1 = mapping[entity1]
         real_entity2 = mapping[entity2]
         if (relation, real_entity1, real_entity2) not in knowns:
-            return False
-    return True
+            return False, [(relation, mapping[entity1], mapping[entity2]) for relation, entity1, entity2 in rule]
+    return True, [(relation, mapping[entity1], mapping[entity2]) for relation, entity1, entity2 in rule]
 
 # Helper function for backtracking to try all possible mappings
 @jit(nopython=True)
-def try_mapping(rule, knowns, mapping, number_pseudo_entities, idx=0):
+def try_mapping(rule, knowns, mapping, number_pseudo_entities, rule_index, idx=0):
     if idx == number_pseudo_entities:
         # If we've tried all pseudo entities and found a valid mapping, check the rule
         return check_mapping(rule, mapping, knowns)
 
     for entity in range(number):
+        if idx == 1 and entity == mapping[0]:
+            continue
+        if idx == 1 and (rule_index, mapping[0], entity) in knowns:
+            continue
         mapping[idx] = entity
-        if try_mapping(rule, knowns, mapping, number_pseudo_entities, idx + 1):
-            return True
-    return False
+        answer, used_relation = try_mapping(rule, knowns, mapping, number_pseudo_entities, rule_index, idx + 1)
+        if answer:
+            return answer, used_relation
+    return False, used_relation
 
 # deduce all known relations
 i = 0
+unused_relation = known_relations.copy()
+used_rule = np.zeros(len(All_Rules))
 while True:
     print(f"Round {i}")
     i += 1
     new_relations = set()
-    for rule_index, Rule in All_Rules:
+    for j, (rule_index, Rule) in enumerate(tqdm(All_Rules)):
         # Extract pseudo entities from the rule
         number_pseudo_entities = len(set(Rule[:, 1:3].flatten()))
 
         # Try mapping pseudo entities to real entities
         mapping = np.zeros(number_pseudo_entities, dtype=np.int64) # Initialize the mapping
-        if try_mapping(Rule, known_relations, mapping, number_pseudo_entities):
+        answer, used_relation = try_mapping(Rule, known_relations, mapping, number_pseudo_entities, rule_index)
+        if answer:
             # If a valid mapping is found, deduce the real relations
             # Create the new relation using the mapping of pseudo entities to real ones
             real_entity1 = mapping[0]
             real_entity2 = mapping[1]
-            if real_entity1 is not None and real_entity2 is not None:
-                if (rule_index, real_entity1, real_entity2) not in known_relations:
-                    new_relations.add((rule_index, real_entity1, real_entity2))  # Add the new relation
+            if (rule_index, real_entity1, real_entity2) not in known_relations:
+                unused_relation = unused_relation - set(used_relation)
+                new_relations.add((rule_index, real_entity1, real_entity2))  # Add the new relation
+                used_rule[j] = 1
+    
+    unused_relation = unused_relation - set(new_relations)
 
     print(f"Found {len(new_relations)} new relations")
     if len(new_relations) == 0:
         break
     known_relations.update(new_relations)
+
+print(f"unused relations:", len(unused_relation))
+print(f"used rule:", np.sum(used_rule))
+with open(args.output_rule, 'w') as file:
+    for j, (i, Rule) in enumerate(All_Rules):
+        if used_rule[j] != 1:
+            continue
+        file.write(f"{i}\n")
+        for row in Rule:
+            file.write(" ".join(map(str, row)) + "\n")
+
+
+known_relations = known_relations - unused_relation
+
+known_entities = set()
+for relation in known_relations:
+    known_entities.add(relation[1])
+    known_entities.add(relation[2])
+known_entities = list(known_entities)
+entity_mapping = {entity: i for i, entity in enumerate(known_entities)}
+known_relations = {(relation[0], entity_mapping[relation[1]], entity_mapping[relation[2]]) for relation in known_relations}
+print(f"Generated {len(known_entities)} entities")
 
 # Writing the relations to a file
 with open(args.output_relation, 'w') as file:
